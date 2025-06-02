d934bb167bf5c37e37a589009d9edd20a407ad8b
priyanka-bv-dev <priyanka@fuzionest.com>
2025-06-02 20:52:20 +0530

    filter changes

--- src/app/salesAddEdit.tsx
+++ src/app/salesAddEdit.tsx
@@ -1617,7 +1617,7 @@ export default function Home({ id }: Props) {
                         className={cn(
                           "h-10",
                           "hover:border-black mt-5",
-                         
+                          "font-semibold",
 
                           orderValidation.customer ? "border-red-500" : "",
                         )}
